ae7d8242f8899444cf08a38c42bf48c56599cd2d
Shapin199 <mdtanvir054321@gmail.com>
2025-04-06 02:07:34 +0600

    mongoose version

--- backend/src/lib/db.js
+++ backend/src/lib/db.js
@@ -5,8 +5,8 @@ export const connectDB = async () => {
     try {
         //console.log(process.env.MONGODB_URI);
         const conn = await mongoose.connect(process.env.MONGODB_URI)
-        console.log(conn);
-        //console.log(`MongoDB Connected: ${conn.connection.host}`)
+       
+        console.log(`MongoDB Connected: ${conn.connection.host}`)
     } catch (error) {
         console.log(error);
         process.exit(1);
